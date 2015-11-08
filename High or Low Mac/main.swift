4f853367761240be766b0808cfbdaeeb5d5f0cef
Glenn Hancock <spudhead42@hotmail.com>
2015-11-08 15:32:45 +0900

    Initial Commit

---
+++ High or Low Mac/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  High or Low Mac
+//
+//  Created by Glenn Hancock on 11/8/15.
+//  Copyright © 2015 Glenn Hancock. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
