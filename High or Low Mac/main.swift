adf920095105efa0c1c7d1b41d39e26edad43960
Glenn Hancock <spudhead42@hotmail.com>
2015-11-08 16:31:50 +0900

    finished

--- High or Low Mac/main.swift
+++ High or Low Mac/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+let answer = randomIntBetween(1, high: 100)
+var turn = 1
 
+while(true) {
+    print("Guess #\(turn): Enter a number between 1 and 100.")
+    
+    let userInput = input()
+    let inputAsInt = Int(userInput)
+    
+    if let guess = inputAsInt {
+        if(guess > answer) {
+            print("Lower!")
+        } else if(guess < answer) {
+            print("Higher!")
+        } else {
+            print("Correct! The answer was \(answer).")
+            break
+        }
+    }  else  {
+        print("Please enter a valid number.")
+       continue
+    }
+    turn++
+}
+print("It took you \(turn) guesses.")
